c317075d4537075bd266c8f7087b7b87300d6909
Sathish S <sathish_nec@yahoo.co.in>
2014-02-27 22:02:44 +0000

    Initial Symfony Plain commit

---
+++ app/cache/dev/twig/52/15/eb0bd1da78c0498f1de4085d839dfca3d0c803e43761d7d0f325b0f71d8a.php
@@ -0,0 +1,115 @@
+<?php
+
+/* @WebProfiler/Profiler/toolbar_js.html.twig */
+class __TwigTemplate_5215eb0bd1da78c0498f1de4085d839dfca3d0c803e43761d7d0f325b0f71d8a extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<div id=\"sfwdt";
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "\" class=\"sf-toolbar\" style=\"display: none\"></div>
+";
+        // line 2
+        $this->env->loadTemplate("@WebProfiler/Profiler/base_js.html.twig")->display($context);
+        // line 3
+        echo "<script>/*<![CDATA[*/
+    (function () {
+        ";
+        // line 5
+        if (("top" == (isset($context["position"]) ? $context["position"] : $this->getContext($context, "position")))) {
+            // line 6
+            echo "            var sfwdt = document.getElementById('sfwdt";
+            echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+            echo "');
+            document.body.insertBefore(
+                document.body.removeChild(sfwdt),
+                document.body.firstChild
+            );
+        ";
+        }
+        // line 12
+        echo "
+        Sfjs.load(
+            'sfwdt";
+        // line 14
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "',
+            '";
+        // line 15
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_wdt", array("token" => (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")))), "html", null, true);
+        echo "',
+            function(xhr, el) {
+                el.style.display = -1 !== xhr.responseText.indexOf('sf-toolbarreset') ? 'block' : 'none';
+
+                if (el.style.display == 'none') {
+                    return;
+                }
+
+                if (Sfjs.getPreference('toolbar/displayState') == 'none') {
+                    document.getElementById('sfToolbarMainContent-";
+        // line 24
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'none';
+                    document.getElementById('sfToolbarClearer-";
+        // line 25
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'none';
+                    document.getElementById('sfMiniToolbar-";
+        // line 26
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'block';
+                } else {
+                    document.getElementById('sfToolbarMainContent-";
+        // line 28
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'block';
+                    document.getElementById('sfToolbarClearer-";
+        // line 29
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'block';
+                    document.getElementById('sfMiniToolbar-";
+        // line 30
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'none';
+                }
+            },
+            function(xhr) {
+                if (xhr.status !== 0) {
+                    confirm('An error occurred while loading the web debug toolbar (' + xhr.status + ': ' + xhr.statusText + ').\\n\\nDo you want to open the profiler?') && (window.location = '";
+        // line 35
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_profiler", array("token" => (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")))), "html", null, true);
+        echo "');
+                }
+            }
+        );
+    })();
+/*]]>*/</script>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "@WebProfiler/Profiler/toolbar_js.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  91 => 35,  83 => 30,  79 => 29,  75 => 28,  70 => 26,  66 => 25,  42 => 12,  30 => 5,  26 => 3,  24 => 2,  19 => 1,  98 => 40,  93 => 9,  80 => 41,  46 => 14,  44 => 9,  40 => 8,  36 => 7,  32 => 6,  27 => 4,  22 => 1,  64 => 11,  55 => 12,  53 => 11,  50 => 15,  47 => 9,  41 => 7,  115 => 40,  112 => 39,  110 => 38,  107 => 37,  101 => 33,  92 => 31,  88 => 6,  82 => 26,  78 => 40,  74 => 22,  72 => 21,  67 => 18,  65 => 17,  62 => 24,  58 => 13,  49 => 12,  45 => 11,  38 => 7,  34 => 4,  31 => 3,  28 => 3,);
+    }
+}
---
+++ app/cache/dev/twig/a9/df/9289436dc169d8c1e62945226da83ff35a95f7340f1c1675c6998d1dbea6.php
@@ -0,0 +1,133 @@
+<?php
+
+/* @WebProfiler/Profiler/base_js.html.twig */
+class __TwigTemplate_a9df9289436dc169d8c1e62945226da83ff35a95f7340f1c1675c6998d1dbea6 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<script>/*<![CDATA[*/
+    Sfjs = (function() {
+        \"use strict\";
+
+        var noop = function() {},
+
+            profilerStorageKey = 'sf2/profiler/',
+
+            request = function(url, onSuccess, onError, payload, options) {
+                var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+                options = options || {};
+                xhr.open(options.method || 'GET', url, true);
+                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                xhr.onreadystatechange = function(state) {
+                    if (4 === xhr.readyState && 200 === xhr.status) {
+                        (onSuccess || noop)(xhr);
+                    } else if (4 === xhr.readyState && xhr.status != 200) {
+                        (onError || noop)(xhr);
+                    }
+                };
+                xhr.send(payload || '');
+            },
+
+            hasClass = function(el, klass) {
+                return el.className.match(new RegExp('\\\\b' + klass + '\\\\b'));
+            },
+
+            removeClass = function(el, klass) {
+                el.className = el.className.replace(new RegExp('\\\\b' + klass + '\\\\b'), ' ');
+            },
+
+            addClass = function(el, klass) {
+                if (!hasClass(el, klass)) { el.className += \" \" + klass; }
+            },
+
+            getPreference = function(name) {
+                if (!window.localStorage) {
+                    return null;
+                }
+
+                return localStorage.getItem(profilerStorageKey + name);
+            },
+
+            setPreference = function(name, value) {
+                if (!window.localStorage) {
+                    return null;
+                }
+
+                localStorage.setItem(profilerStorageKey + name, value);
+            };
+
+        return {
+            hasClass: hasClass,
+
+            removeClass: removeClass,
+
+            addClass: addClass,
+
+            getPreference: getPreference,
+
+            setPreference: setPreference,
+
+            request: request,
+
+            load: function(selector, url, onSuccess, onError, options) {
+                var el = document.getElementById(selector);
+
+                if (el && el.getAttribute('data-sfurl') !== url) {
+                    request(
+                        url,
+                        function(xhr) {
+                            el.innerHTML = xhr.responseText;
+                            el.setAttribute('data-sfurl', url);
+                            removeClass(el, 'loading');
+                            (onSuccess || noop)(xhr, el);
+                        },
+                        function(xhr) { (onError || noop)(xhr, el); },
+                        options
+                    );
+                }
+
+                return this;
+            },
+
+            toggle: function(selector, elOn, elOff) {
+                var i,
+                    style,
+                    tmp = elOn.style.display,
+                    el = document.getElementById(selector);
+
+                elOn.style.display = elOff.style.display;
+                elOff.style.display = tmp;
+
+                if (el) {
+                    el.style.display = 'none' === tmp ? 'none' : 'block';
+                }
+
+                return this;
+            }
+        }
+    })();
+/*]]>*/</script>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "@WebProfiler/Profiler/base_js.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  91 => 35,  83 => 30,  79 => 29,  75 => 28,  70 => 26,  66 => 25,  42 => 12,  30 => 5,  26 => 3,  24 => 2,  19 => 1,  98 => 40,  93 => 9,  80 => 41,  46 => 14,  44 => 9,  40 => 8,  36 => 7,  32 => 6,  27 => 4,  22 => 1,  64 => 11,  55 => 12,  53 => 11,  50 => 15,  47 => 9,  41 => 7,  115 => 40,  112 => 39,  110 => 38,  107 => 37,  101 => 33,  92 => 31,  88 => 6,  82 => 26,  78 => 40,  74 => 22,  72 => 21,  67 => 18,  65 => 17,  62 => 24,  58 => 13,  49 => 12,  45 => 11,  38 => 7,  34 => 4,  31 => 3,  28 => 3,);
+    }
+}
